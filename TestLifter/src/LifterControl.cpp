01114f31251c08139772a8261b731d4ff0406c3b
BEAST <16owend@elmbrookstudents.org>
2015-01-24 13:19:14 -0600

    Fin lifter control

--- TestLifter/src/LifterControl.cpp
+++ TestLifter/src/LifterControl.cpp
@@ -15,51 +15,110 @@
 #define LEVEL_3 900
 #define MOTOR_SPEED 0.1
 
-
 class LifterControl {
 
-
-	Encoder *en1 = new Encoder(0, 1, true, Encoder::k2X);
+	Encoder *en1;
 	Talon *lifter;
+	DigitalInput *upperLimit;
+	DigitalInput *lowerLimit;
 
 public:
 
-
 	LifterControl() {
+		en1 = new Encoder(0, 1, true, Encoder::k2X);
+		lifter = new Talon(6);
 		en1->Reset();
+		level1Value = LEVEL_1;
+		level2Value = LEVEL_2;
+		level3Value = LEVEL_3;
+		homeValue = HOME;
+		upperLimit = new DigitalInput(3);
+		lowerLimit = new DigitalInput(4);
+	}
+
+	void Stop() {
+		lifter->Set(0);
+	}
+
+	void MoveToHome() {
+		if (lowerLimit->Get()) {
+			MoveToHomePrivate();
+		}
 	}
 
 	void MoveToLevel1() {
-		if (en1->Get() < LEVEL_1 && en1->Get() > LEVEL_1 + TOLERANCE) {
-			lifter->Set(MOTOR_SPEED);
-			MoveToLevel1();
-		} else if (en1->Get() > LEVEL_1 && en1->Get() < LEVEL_1 - TOLERANCE) {
-			lifter->Set(-MOTOR_SPEED);
-			MoveToLevel1();
-		} else {
-			lifter->Set(0);
+		if (upperLimit->Get()) {
+			MoveToLevel1Private();
 		}
 	}
 
 	void MoveToLevel2() {
-		if (en1->Get() < LEVEL_2 && en1->Get() > LEVEL_2 + TOLERANCE) {
-			lifter->Set(MOTOR_SPEED);
-			MoveToLevel2();
-		} else if (en1->Get() > LEVEL_2 && en1->Get() < LEVEL_2 - TOLERANCE) {
+		if (upperLimit->Get()) {
+			MoveToLevel2Private();
+		}
+	}
+
+	void MoveToLevel3() {
+		if (upperLimit->Get()) {
+			MoveToLevel3Private();
+		}
+	}
+
+	void SetHome(int homeValue) {
+		this->homeValue = homeValue;
+	}
+
+	void SetLevel1(int level1Value) {
+		this->level1Value = level1Value;
+	}
+
+	void SetLevel2(int level1Value) {
+		this->level2Value = level2Value;
+	}
+
+	void SetLevel3(int level1Value) {
+		this->level3Value = level3Value;
+	}
+
+private:
+	int level1Value;
+	int level2Value;
+	int level3Value;
+	int homeValue;
+
+	void MoveToHomePrivate() {
+		if (en1->Get() > HOME + TOLERANCE) {
 			lifter->Set(-MOTOR_SPEED);
-			MoveToLevel2();
 		} else {
 			lifter->Set(0);
 		}
 	}
 
-	void MoveToLevel3() {
-		if (en1->Get() < LEVEL_3 && en1->Get() > LEVEL_3 + TOLERANCE) {
+	void MoveToLevel1Private() {
+		if (en1->Get() < LEVEL_1 - TOLERANCE) {
 			lifter->Set(MOTOR_SPEED);
-			MoveToLevel3();
-		} else if (en1->Get() > LEVEL_3 && en1->Get() < LEVEL_3 - TOLERANCE) {
+		} else if (en1->Get() > LEVEL_1 + TOLERANCE) {
 			lifter->Set(-MOTOR_SPEED);
-			MoveToLevel3();
+		} else {
+			lifter->Set(0);
+		}
+	}
+
+	void MoveToLevel2Private() {
+		if (en1->Get() < LEVEL_2 - TOLERANCE) {
+			lifter->Set(MOTOR_SPEED);
+		} else if (en1->Get() > LEVEL_2 + TOLERANCE) {
+			lifter->Set(-MOTOR_SPEED);
+		} else {
+			lifter->Set(0);
+		}
+	}
+
+	void MoveToLevel3Private() {
+		if (en1->Get() < LEVEL_3 - TOLERANCE) {
+			lifter->Set(MOTOR_SPEED);
+		} else if (en1->Get() > LEVEL_3 + TOLERANCE) {
+			lifter->Set(-MOTOR_SPEED);
 		} else {
 			lifter->Set(0);
 		}
--- TestLifter/src/RobotLifter.cpp
+++ TestLifter/src/RobotLifter.cpp
@@ -5,8 +5,7 @@
 #include <RobotBase.h>
 #include <RobotDrive.h>
 
-class Robot: public IterativeRobot
-{
+class Robot: public IterativeRobot {
 
 	RobotDrive myRobot;
 	XboxController *xbox;
@@ -17,60 +16,84 @@ class Robot: public IterativeRobot
 
 public:
 	Robot() :
-		myRobot(0,1), // these must be initialized in the same order
-		lw(NULL),
-		autoLoopCounter(0)
-	{
+			myRobot(0, 1), // these must be initialized in the same order
+			lw(NULL), autoLoopCounter(0) {
 		myRobot.SetExpiration(0.1);
 		xbox = XboxController::getInstance();
 		lifter = new LifterControl();
+		lifterMotor = new Talon(5);
 		lifterMotor->EnableDeadbandElimination(true);
-		lifterMotor = new Talon(6);
 	}
 
-
 private:
-	void RobotInit()
-	{
+	void RobotInit() {
 		lw = LiveWindow::GetInstance();
 	}
 
-	void AutonomousInit()
-	{
+	void AutonomousInit() {
 		autoLoopCounter = 0;
 	}
 
-	void AutonomousPeriodic()
-	{
-		if(autoLoopCounter < 100) //Check if we've completed 100 loops (approximately 2 seconds)
-		{
+	void AutonomousPeriodic() {
+		if (autoLoopCounter < 100) //Check if we've completed 100 loops (approximately 2 seconds)
+				{
 			myRobot.Drive(-0.5, 0.0); 	// drive forwards half speed
 			autoLoopCounter++;
-			} else {
+		} else {
 			myRobot.Drive(0.0, 0.0); 	// stop robot
 		}
 	}
 
-	void TeleopInit()
-	{
+	void TeleopInit() {
 
 	}
 
-	void TeleopPeriodic()
-	{
+	void TeleopPeriodic() {
 		myRobot.ArcadeDrive(xbox->getLeftStick()); // drive with arcade style (use right stick)
 
-		if (xbox->isBPressed()) {
+		SmartDashboard::PutNumber("Home Value", 0.0);
+		SmartDashboard::PutNumber("Level 1 Value", 0.0);
+		SmartDashboard::PutNumber("Level 2 Value", 0.0);
+		SmartDashboard::PutNumber("Level 3 Value", 0.0);
+		SmartDashboard::PutBoolean("Use Values", false);
+
+		double home = SmartDashboard::GetNumber("HomeValue");
+		double level1 = SmartDashboard::GetNumber("Level 1 Value");
+		double level2 = SmartDashboard::GetNumber("Level 2 Value");
+		double level3 = SmartDashboard::GetNumber("Level 3 Value");
+
+		if (SmartDashboard::GetBoolean("Use Values")) {
+			lifter->SetHome((int) home);
+			lifter->SetLevel1((int) level1);
+			lifter->SetLevel2((int) level2);
+			lifter->SetLevel3((int) level3);
+		}
+
+	/*
+		if (xbox->isBHeld()) {
 			lifterMotor->Set(-0.25);
-		} else if (xbox->isXPressed()) {
+		} else if (xbox->isXHeld()) {
 			lifterMotor->Set(0.25);
 		} else {
 			lifterMotor->Set(0);
 		}
+	*/
+
+		if (xbox->isBHeld()) {
+			lifter->MoveToLevel1();
+		} else if (xbox->isYHeld()) {
+			lifter->MoveToLevel2();
+		} else if (xbox->isXHeld()) {
+			lifter->MoveToLevel3();
+		} else if (xbox->isAHeld()) {
+			lifter->MoveToHome();
+		} else {
+			lifter->Stop();
+		}
+
 	}
 
-	void TestPeriodic()
-	{
+	void TestPeriodic() {
 
 		lw->Run();
 	}
