db07892fbafa75df960fff03a9dcbd7646d6bfdd
DaSpook <gummaditanay@gmail.com>
2015-01-26 20:27:48 -0600

    Recommit Meccanum drive

--- Mecanum Drive/src/Robot.cpp
+++ Mecanum Drive/src/Robot.cpp
@@ -16,13 +16,17 @@ class Robot: public SampleRobot
 
 	RobotDrive robotDrive;	// robot drive system
 	Joystick stick;			// only joystick
+	XboxController *controller;
 
 public:
 	Robot() :
 			robotDrive(frontLeftChannel, rearLeftChannel,
 					   frontRightChannel, rearRightChannel),	// these must be initialized in the same order
 			stick(joystickChannel)								// as they are declared above.
+
+
 	{
+		controller = XboxController::getInstance();
 		robotDrive.SetExpiration(0.1);
 		robotDrive.SetInvertedMotor(RobotDrive::kFrontLeftMotor, true);	// invert the left side motors
 		robotDrive.SetInvertedMotor(RobotDrive::kRearLeftMotor, true);	// you may need to change or remove this to match your robot
@@ -38,7 +42,7 @@ public:
 		{
         	// Use the joystick X axis for lateral movement, Y axis for forward movement, and Z axis for rotation.
         	// This sample does not use field-oriented drive, so the gyro input is set to zero.
-			robotDrive.MecanumDrive_Cartesian(stick.GetX(), stick.GetY(), stick.GetZ());
+			robotDrive.MecanumDrive_Cartesian(controller->getAxisLeftX(), controller->getAxisTrigger(), controller->getAxisRightX());
 
 			Wait(0.005); // wait 5ms to avoid hogging CPU cycles
 		}
