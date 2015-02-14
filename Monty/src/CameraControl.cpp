84cd9746f746b4d4cfee18fa6e97f675347bc632
Tom Lazar <16lazart@elmbrookstudents.org>
2015-02-14 09:46:58 -0600

    MORe Camrera STUFF

--- Monty/src/CameraControl.cpp
+++ Monty/src/CameraControl.cpp
@@ -21,30 +21,22 @@ public:
 	}
 
 	void RobotInit() {
-		// create an image
 		frame = imaqCreateImage(IMAQ_IMAGE_RGB, 0);
-		//the camera name (ex "cam0") can be found through the roborio web interface
-		imaqError = IMAQdxOpenCamera("cam0", IMAQdxCameraControlModeController, &session);
-		if(imaqError != IMAQdxErrorSuccess) {
-			DriverStation::ReportError("IMAQdxOpenCamera error: " + std::to_string((long)imaqError) + "\n");
-		}
 		imaqError = IMAQdxConfigureGrab(session);
 		if(imaqError != IMAQdxErrorSuccess) {
 			DriverStation::ReportError("IMAQdxConfigureGrab error: " + std::to_string((long)imaqError) + "\n");
 		}
 		IMAQdxStartAcquisition(session);
 
-		//Capture Of Image
-		while(true) {
-			IMAQdxGrab(session, frame, true, NULL);
-			if(imaqError != IMAQdxErrorSuccess) {
-				DriverStation::ReportError("IMAQdxGrab error: " + std::to_string((long)imaqError) + "\n");
-			} else {
-				imaqDrawShapeOnImage(frame, frame, { 10, 10, 100, 100 }, DrawMode::IMAQ_DRAW_VALUE, ShapeMode::IMAQ_SHAPE_OVAL, 0.0f);
-				CameraServer::GetInstance()->SetImage(frame);
-			}
-			Wait(0.005);
-
+	}
+	void TeleopPeriodic(){
+		IMAQdxGrab(session, frame, true, NULL);
+		if(imaqError != IMAQdxErrorSuccess) {
+			DriverStation::ReportError("IMAQdxGrab error: " + std::to_string((long)imaqError) + "\n");
+		} else {
+			imaqDrawShapeOnImage(frame, frame, { 10, 10, 100, 100 }, DrawMode::IMAQ_DRAW_VALUE, ShapeMode::IMAQ_SHAPE_OVAL, 0.0f);
+			CameraServer::GetInstance()->SetImage(frame);
 		}
-	};
+	}
+};
 
